a99141f6aa3a8993bc7ce85ba6c143d258594dc6
Nancy Boktor <nancyboktor1@gmail.com>
2021-09-15 09:19:46 -0400

    update  assertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,4 +1,4 @@
-const eqArrays = function (array1, array2) {
+const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
     return false;
   }
@@ -10,13 +10,12 @@ const eqArrays = function (array1, array2) {
   return true;
 };
 
-const assertArraysEqual = function (array1, array2) {
+const assertArraysEqual = function(array1, array2) {
   if (eqArrays(array1, array2)) {
-    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`)
+    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
   } else {
     console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
   }
 };
-
 
 assertArraysEqual([1, 2, 3], [1, 2, 3]);
