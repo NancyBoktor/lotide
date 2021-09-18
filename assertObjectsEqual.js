56d0d89cdc894d922a402296925ec2e5114c256f
Nancy Boktor <nancyboktor1@gmail.com>
2021-09-18 16:10:07 -0400

    export the assertObjectEqual function

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -50,8 +50,9 @@ const assertObjectsEqual = function (actual, expected) {
   }
 };
 
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
+//Testing for the code before exports
+//const cd = { c: "1", d: ["2", 3] };
+//const dc = { d: ["2", 3], c: "1" };
 
-assertObjectsEqual(cd, dc);
-
+//assertObjectsEqual(cd, dc);
+module.exports = assertObjectsEqual;
