d6f12e1588bc7b64a2a9fa77a934d12be066e876
Nancy Boktor <nancyboktor1@gmail.com>
2021-09-18 20:43:59 -0400

    assertEqual & letterPositions

--- assertEqual.js
+++ assertEqual.js
@@ -6,9 +6,9 @@ const assertEqual = function (actual, expected) {
   }
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual("Lighthouse Labs", "Lighthouse Labs");
-assertEqual(1, 1);
-assertEqual(5, 1);
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual("Lighthouse Labs", "Lighthouse Labs");
+// assertEqual(1, 1);
+// assertEqual(5, 1);
 
 module.exports = assertEqual;
--- letterPositions.js
+++ letterPositions.js
@@ -32,6 +32,6 @@ const letterPositions = function (sentence) {
 };
 
 assertArraysEqual(letterPositions("hello")["h"], [0]);
-assertArraysEqual(letterPositions("hello")["e"], [1]);
+assertArraysEqual(letterPositions("hello").e, [1]);
 assertArraysEqual(letterPositions("hello")["l"], [2, 3]);
 assertArraysEqual(letterPositions("hello")["o"], [4]);
