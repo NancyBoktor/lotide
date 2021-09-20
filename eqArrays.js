95f635a3babe250e24eac7abef953d11ce21fafb
Nancy Boktor <nancyboktor1@gmail.com>
2021-09-20 09:08:24 -0400

    update by import assertEqual

--- eqArrays.js
+++ eqArrays.js
@@ -1,7 +1,10 @@
-// Implement a function eqArrays which takes in two arrays and returns 
-//true or false, based on a perfect match. 
+// import from assertEqual file.
+const assertEqual = require("./assertEqual");
 
-const eqArrays = function(array1, array2) {
+// Implement a function eqArrays which takes in two arrays and returns
+//true or false, based on a perfect match.
+
+const eqArrays = function (array1, array2) {
   if (array1.length !== array2.length) {
     return false;
   }
@@ -12,17 +15,9 @@ const eqArrays = function(array1, array2) {
   }
   return true;
 };
- 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-    
-};
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]) , true);
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true);
