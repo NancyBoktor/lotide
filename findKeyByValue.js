282778e60a9753e11d600fdf5d7c3c83af68af15
Nancy Boktor <nancyboktor1@gmail.com>
2021-09-20 09:27:39 -0400

    update by importing assertEqual

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,12 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+//import from asserEqual file.
+const assertEqual = require("./assertEqual");
 
-const findKeyByValue = function(obj, value) {
+const findKeyByValue = function (obj, value) {
   for (const key in obj) {
     if (obj[key] === value) {
       return key;
@@ -15,7 +10,7 @@ const findKeyByValue = function(obj, value) {
 };
 
 const bestTVShowsByGenre = {
-  'sci_fi': "The Expanse",
+  "sci_fi": "The Expanse",
   comedy: "Brooklyn Nine-Nine",
   drama: "The Wire",
 };
