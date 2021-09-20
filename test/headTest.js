cd6380cf612d526c25b1c9efa22116f5113d2aec
Nancy Boktor <nancyboktor1@gmail.com>
2021-09-20 17:47:36 -0400

    update our head function by testing it with with Mocha and Chai (M

--- test/headTest.js
+++ test/headTest.js
@@ -1,9 +1,22 @@
-//import asserEqual function.
-const assertEqual = require("../assertEqual");
 //import head function.
 const head = require("../head");
+// import mocha
+const assert = require("chai").assert;
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([9]), 9);
-assertEqual(head([]), undefined);
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(["5"]), "5");
+  });
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+  it("returns 9 for [9]", () => {
+    assert.strictEqual(head([9]), 9);
+  });
+  it("returns undefined for []", () => {
+    assert.strictEqual(head([]), undefined);
+  });
+});
