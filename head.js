ae70dcccc48997c3145174f9c34d361b96d7a881
Nancy Boktor <nancyboktor1@gmail.com>
2021-09-20 09:29:46 -0400

    update by importing assertEqual

--- head.js
+++ head.js
@@ -1,17 +1,11 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+//import from asserEqual file.
+const assertEqual = require("./assertEqual");
 
 const head = function (array) {
   return array[0];
 };
 
-
-assertEqual(head([5,6,7]), 5);
+assertEqual(head([5, 6, 7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 assertEqual(head([9]), 9);
 assertEqual(head([]), undefined);
